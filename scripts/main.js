f9632cb70f5dc486dbcdf5e0494210eb17f44851
unknown <eng.r3ssam@gmail.com>
2025-12-30 01:05:20 +0200

    Add view profile button

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+window.onload =function(){
+let profileBtn = document.getElementById("myBtn");
+profileBtn.onclick = function () {
+    window.open("https://github.com/engr3ssam-arch","_blank") ; 
+};
+};
